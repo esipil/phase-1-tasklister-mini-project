6be31b2b0e58afce4b34bac16abede1808a40cbe
Timothy <lemayiant4@gmail.com>
2022-08-28 19:54:04 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskCreater = document.getElementById('create-task-form');
+  taskCreater.addEventListener('submit', logging);
 });
+
+function logging (event) {
+  const input = document.getElementById('new-task-description').value;
+  const todos = document.getElementById('list');
+  const li = document.createElement('li');
+  const btn =document.createElement('button');
+  li.textContent= input
+  btn.textContent= 'x'
+  li.appendChild(btn)
+  li.addEventListener('click', function () {
+    li.remove();
+  })
+  todos.append(li);
+  document.getElementById('create-task-form').reset();
+  event.preventDefault();
+}
